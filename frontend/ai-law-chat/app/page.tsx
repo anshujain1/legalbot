38d58fe295d291e464a0943e1b7a43460ff4bb9e
Anshu Jain <anshujain2515@gmail.com>
2026-01-14 22:06:14 +0530

    chore: initialize Next.js frontend for AI law chatbot

---
+++ frontend/ai-law-chat/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Landingpage from '../components/landingpage'
+const page = () => {
+  return (
+    <div>
+      <Landingpage/>
+    </div>
+  )
+}
+
+export default page
